ad3ac307d967a2281ed9e7d0f5f098d474396bee
betty <bettylin25@gmail.com>
2020-04-24 16:42:38 +0800

    insert event

--- selectTable.sql
+++ selectTable.sql
@@ -18,3 +18,15 @@ go
 
 select * from Wish
 go
+select * from Wish where userId=3
+go
+select * from Event
+select * from Profile
+go
+
+select * from ProfileDetail
+go
+
+insert into Event(productId,eventName,content,startDate,endDate) values(7,'EventTest','discount','20071207','20071207')
+go
+insert into Event(productId,eventName,content,startDate,endDate) values(2,'EventTest2','discount2','20200424','20200429')
