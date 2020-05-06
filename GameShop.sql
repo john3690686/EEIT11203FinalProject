6093d1bf969f82fc4b9691aa2fc00cbce71a550d
john3690686 <john3690686@gmail.com>
2020-05-07 03:17:40 +0800

    我的文章 變更CSS顏色

--- GameShop.sql
+++ GameShop.sql
@@ -58,122 +58,122 @@ go
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'DARK SOULS: REMASTERED','有一天，燃起了第一團火。 重新體驗話題性、代表性十足的經典遊戲。 經過畫質提升，將以驚豔的60fps形式呈現，做好重返羅德蘭的準備吧。 黑暗靈魂 Remastered包含遊戲本體，以及DLC《Artorias of the Abyss》。 ','1000','動作',
 '2020-03-02','2020-04-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale1.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale1.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Terraria','Dig, fight, explore, build! Nothing is impossible in this action-packed adventure game. Four Pack also available! ','800','角色扮演',
 '2020-03-12','2020-05-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale2.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale2.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Resident Evil3','不管逃到哪裡也會被追至絕路。 拉昆市發生突如其來的生物災害，更為巨大的威脅被投入至這個逐漸崩潰的城鎮中…… 多重的危機、九死一生的驚險動作和重新建構的逃生劇，一一超乎你的預想。 你能逃離這片絕望嗎？ 同時收錄《Resident Evil Resistance》。','1790','恐怖',
 '2020-03-20','2020-07-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale3.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale3.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Age of Empires II: Definitive Edition','有史以來最受歡迎的戰略遊戲之一迎來 20 週年，為此我們推出了《Age of Empires II: Definitive Edition》。本作擁有驚人的 4K 超高畫質，全面重置的新原聲音樂，以及內含 3 個新戰役和 4 個新文明的全新內容：《最後的可汗》。 ','600','策略',
 '2020-03-12','2020-07-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale4.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale4.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Monster Hunter','新的生命之地。狩獵, 就是本能! 在最新作「Monster Hunter: World」中, 玩家可以體驗終極的狩獵生活, 活用新建構的世界中各種各樣的地形 與生態環境享受狩獵的驚喜與興奮。','1800','角色扮演',
 '2020-04-12','2020-07-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale5.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale5.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Sekiro: Shadows Die Twice','在由開發商FromSoftware（Dark Souls系列的製作單位）的全新歷險中開拓你的復仇之路。 勇猛復仇，挽回榮譽，巧妙殺敵。 ','1033','動作',
 '2020-04-12','2020-07-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale6.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale6.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'For The King','For The King是一款結合桌遊和 roguelike 類型元素的跨越領域戰略型 RPG 遊戲。可以在線和單機進行單人或多人合作的遊戲體驗。','318','策略',
 '2020-04-12','2020-08-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale7.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale7.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Mount & Blade II: Bannerlord','The horns sound, the ravens gather. An empire is torn by civil war. Beyond its borders, new kingdoms rise. Gird on your sword, don your armour, summon your followers and ride forth to 
   win glory on the battlefields of Calradia. Establish your hegemony and create a new world out of the ashes of the old.','1399','策略',
 '2020-04-12','2020-08-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale8.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale8.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'The Witcher 3: Wild Hunt','當戰火在北方領域延燒，傑洛特將接下終其一生最重大的委託：追尋可以撼動世界的武器，預言之子的下落。','868','角色扮演',
 '2020-04-12','2020-08-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale9.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale9.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Divinity: Original Sin 2','The eagerly anticipated sequel to the award-winning RPG. Gather your party. Master deep, tactical combat. Join up to 3 other players - but know that only one of you will have the chance 
   to become a God.','1038','角色扮演',
 '2020-05-12','2020-09-30',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\sale10.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale10.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Dead by Daylight','《Dead by Daylight》是一款多人（4對1）恐怖遊戲，由一名玩家扮殘暴殺手，其餘的四名玩家則扮演逃生者。逃生者試圖從殺手的魔爪下逃亡，避免被他捉住、折磨及殺死。',' 468','恐怖',
 '2020-05-03','2020-05-30',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale11.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale11.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'DOOM Eternal','地獄大軍已入侵地球。加入精彩刺激的單人戰役，化身毀滅戰士征服各次元的惡魔，阻止人類迎向最終滅絕。 唯一能嚇阻他們的……就是你。 ','1799','射擊',
 '2020-03-12','2020-06-30',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale12.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale12.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'WWE 2K20','誠邀各位玩家在10月22日登上《WWE 2K20》中的擂台。各位最愛的WWE超級巨星、傳奇巨星、名人堂成員及NXT的最佳選手都將齊聚盛典，共慶《WWE 2K》系列重獲新生！《WWE 2K20》將帶來遊戲體驗上的重大革新、更為流暢的控制操作，以及更趣味橫生、極富創意的豐富內容。','1790','運動',
 '2020-05-15','2020-06-30',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale13.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale13.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Euro Truck Simulator 2','Travel across Europe as king of the road, a trucker who delivers important cargo across impressive distances! With dozens of cities to explore, your endurance, skill and speed will all be pushed to their limits. ','468','模擬',
 '2020-04-12','2020-05-09',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale14.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale14.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Farming Simulator 19','百萬銷量的品牌將以全新圖形引擎燃爆回歸，讓人上癮的遊戲玩法以及身臨其境的視效，邀請玩家體驗有史以來最大型的農耕模擬遊戲！','690','模擬',
 '2020-04-30','2020-05-30',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale15.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale15.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Green Hell','《Green Hell》是悶熱的亞馬遜雨林中上演的一場求生奮鬥。掙扎求生的玩家將面臨耐力的極限挑戰，因為孤獨感不只對身體，更會對心靈構成重大考驗。 ','378','模擬',
 '2020-03-12','2020-04-15',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale16.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale16.jpg', Single_Blob)as gamepic
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'The Complex','After a major bio-weapon attack on London, two scientists find themselves in a locked-down laboratory with time, and air, running out. The Complex is an interactive sci-fi thriller movie  where your decisions lead to one of eight suspenseful endings.','238','冒險',
 '2020-05-12','2020-06-12',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale17.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale17.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Sentinels of Freedom','Prepare for a story-driven campaign within the Sentinel Comics universe as you join forces with an ever-growing team of heroes to face a wide variety of villains. What seems like a  typical bank heist reveals itself to be just the beginning of a much more sinister plan! ','378','冒險',
 '2020-03-06','2020-05-15',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale18.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale18.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'Circle Empires Rivals','In this fast-paced multiplayer RTS, you and your friends will take control of your own tiny kingdoms and conquer a world made of circles. Each one is overflowing with enemies to fight, monsters to hunt and loot to hoard – but watch out... Friends can quickly become rivals!','268','休閒',
 '2020-05-01','2020-08-30',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale19.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale19.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'DJMAX RESPECT V','DJMAX RESPECT V is the latest installment from the creators of the critically acclaimed rhythm game franchise ‘DJMAX.’ Experience tracks from popular artists such as Marshmello, Porter  Robinson, and Yukika, exclusive Full HD Music Videos, online multiplayer, new game modes, and more!','698','休閒',
 '2020-05-08','2020-09-30',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale20.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale20.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'NBA 2K20','2K continues to redefine what’s possible in sports gaming with NBA 2K20, featuring best in class graphics & gameplay, ground breaking game modes, and unparalleled player control and  customization.','1790','運動',
 '2020-05-03','2020-06-05',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale21.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale21.jpg', Single_Blob)as gamepic
 go
 INSERT INTO Product(productName,intro,price,tag, uploadTime, downloadTime, productImage)
 select 'F1 2019','F1 2019官方電玩遊戲，挑戰你加入Codemasters史上最野心勃勃的F1R遊戲，極速狂飆，擊敗對手。當季所有官方隊伍、車手與21座賽車場在F1R 2019中齊聚一堂。今年更將迎來F2?，玩家可在2018賽季中一較高下。','858','運動',
 '2020-04-03','2020-06-05',
-BulkColumn from Openrowset( Bulk N'C:\DataSource\teamproject\img\sale22.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\sale22.jpg', Single_Blob)as gamepic
 go
 
 select * from Product
@@ -253,7 +253,7 @@ go
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '5','《魔物獵人》更新擊殺排行榜',
 '本次統計時間為 4 月 17~23 日，僅統計 PS4 版，且不包含聚魔之地。 在擊殺排行中，上週位居首位的雷狼龍本週繼續霸榜冠軍，第二名至第五名分別是殲世滅盡龍、絢輝龍、冥赤龍與炎王龍。','2020-04-17','2020-04-23',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\05.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\05.jpg', Single_Blob)as gamepic
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '3','《惡靈古堡 3 重製版》上市頭 5 天全球出貨突破 200 萬套 下載版佔比近半',
@@ -263,13 +263,13 @@ select '3','《惡靈古堡 3 重製版》上市頭 5 天全球出貨突破 200 
  
 　　官方宣布，《惡靈古堡 3 重製版》於上市頭 5 天的全球出貨套數已達到 200 萬套，其中數位下載版的銷售佔比已經達到約 50% 的水準。而 2019 年 1 月推出的《惡靈古堡 2 重製版》的銷售套數亦成長至 650 萬套。',
 '2020-04-03','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\03.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\03.jpg', Single_Blob)as gamepic
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '7','戰略 RPG《為了國王 For The King》PS4、Switch 中文版即將發售',
 'H2 Interactive 宣布，由 Curve Digital 製作的戰略 RPG《為了國王 For The King》將於 3 月 30 日發售PS4 及 Nintendo Switch 繁體中文版。',
 '2020-03-30','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\07.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\07.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
@@ -277,7 +277,7 @@ select '8','戰爭策略名作《騎馬與砍殺2：霸主》2020年3月登上St
 '結束多年等待，由 TaleWorlds 開發的中世紀動作角色扮演遊戲續作《騎馬與砍殺 2：霸主》（Mount & Blade 2: Bannerlord），在今年德國科隆遊戲展 Gamescom 2019 正式公布遊戲釋出時間點，預計 2020 年 3 月在 Steam 進入「搶先體驗」（Early Access）公開測試。
 經過年初的封閉 Beta 測試活動，這次科隆遊戲展會中，TaleWorlds 在一支全新公開的預告片中證實了《騎馬與砍殺 2：霸主》預計將於 明年 3 月在 Steam 上進入搶先體驗階段。再過半年左右，玩家們就能夠玩到這款令全球玩家望穿秋水的傳奇之作。',
 '2020-03-30','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\08.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\08.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
@@ -286,7 +286,7 @@ select '6','2019 Steam大獎揭曉《隻狼》獲年度遊戲獎',
 
 由黑魂系列團隊 From Software 去年推出的《隻狼：暗影雙死》甫一推出隨即在全球玩家社群造成轟動，本次擊敗與之一同競爭的《惡靈古堡 2 重製版》、《惡魔獵人 5》、《天命 2》、《星際大戰 絕地：組織》，獲得 Steam 年度最佳遊戲實至名歸。',
 '2020-01-01','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\06.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\06.jpg', Single_Blob)as gamepic
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '2','挖掘！製作！戰鬥！《泰拉瑞亞 Terraria》NS 中文版 12 月 19 日發售',
@@ -294,33 +294,33 @@ select '2','挖掘！製作！戰鬥！《泰拉瑞亞 Terraria》NS 中文版 1
  
 　　本作自 2011 年 PC 版發售後，在全世界創下了銷售量達 2,000 萬以上的亮眼成績，Spike Chunsoft 從 2013 年起便在日本的各遊戲平台販售，日本國內累計出貨量・下載數超過 90 萬，可說是受到眾多玩家支持的超人氣作品。而本次的新作 Nintendo Switch 版將會支援 PC 版的最新大型更新檔 Ver. 1.3。',
 '2019-12-19','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\02.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\02.jpg', Single_Blob)as gamepic
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '9','【獵魔士】Netflix新劇效應！《Witcher 3》遊玩人數創新紀錄',
 '《巫師》小說同名原著改編真人電視影集《獵魔士》於 Netfilx 上映後，除了為小說帶來了不小的推廣效果外，也對《巫師》系列的遊戲帶來了不小的影響，光是《巫師 3》（The Witcher 3）在 12 月 25 號的同時在線遊玩人數便創下了 55468 位的佳績。',
 '2019-12-13','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\09.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\09.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '10','《神諭：原罪2》推出第3部免費DLC',
 'Larian Studios 最近公開他們為《神諭：原罪2》推出的第3波免費 DLC 已經登陸PC、PS4 與 Xbox One 和近期發行的 Switch 版本囉。這次的 DLC 名為「秩序與魔法（Order & Magic）」，帶來新的追隨者、新物品、強而有力的咒語、改善與Switch上的好友邀請功能。',
 '2019-11-20','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\10.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\10.jpg', Single_Blob)as gamepic
 go
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '4','【GC 19】《世紀帝國 2：決定版》宣布將於 11 月 14 日正式推出',
 '微軟於今日在 Gamescom 展前所展開的「Inside Xbox - Live @ gamescom 2019」活動上，正式宣布《世紀帝國 2：決定版》將於 11 月 14 日推出的消息。《世紀帝國 2：決定版》為經典即時策略遊戲《世紀帝國 2》的強化版。除了有 4K 超高畫質、以及全新重製音軌等內容外，也增加了 4 個全新文明以及 3 個新章節。隨著這次 gamescom 的展開，官方也宣布遊戲將於 11 月正式上市。',
 '2019-11-14','2020-05-08',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\04.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\04.jpg', Single_Blob)as gamepic
 
 INSERT INTO Event(productId,eventName,content,startDate,endDate, EventImage)
 select '1','《黑暗靈魂 Remastered》將於 5 月 11 日至 5 月 12 日舉行公開測試',
 '台灣萬代南夢宮娛樂今（2）日宣布，FROMSOFTWARE Inc .開發，預定於 2018 年 5 月 24 日正式發售的動作角色扮演遊戲《黑暗靈魂 Remastered》PlayStation 4／Xbox One 版將會舉行網路伺服器功能公開測試，藉由本次大規模的網路壓力測試以驗證連線系統機能，提升正式發售時的連線遊玩品質。',
 '2018-05-11','2018-05-24',
-BulkColumn from Openrowset( Bulk N'C:\Users\Student\Desktop\Final\img\01.jpg', Single_Blob)as gamepic
+BulkColumn from Openrowset( Bulk N'D:\Data\_003_SSHWebWorkSpace\EEIT11203FinalProject\ThirdTeamProject\GameShop\WebContent\img\01.jpg', Single_Blob)as gamepic
 go
 
 
